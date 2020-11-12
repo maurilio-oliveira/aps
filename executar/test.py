c56155902075ca34df8e577d08af9aa7353b40ef
oliver <mauriliodepaulo@gmail.com>
2020-11-12 19:15:22 -0300

    chegada \o/
    meus parabens! esta é a ultima versao de criaçao do codigo
    significa que os testes acabaram e que vc presisa apenas juntar tudo.
    -correçao de bugs com a biblioteca da lista
    -criaçao do arquivo bit.py que faz a conversao de bin -> str
    
                                                            v 0.2.15

--- executar/test.py
+++ executar/test.py
@@ -2,15 +2,19 @@ from junçao import*
 
 #def criptografar():
 """
-    criar_lista #
-    codificar
-    re ordenar
+    criar_lista
+               \
+                : listbin
+               /
+    codificar  
+    re ordenar #sp_list
     binario
-    re ordenar
-    juntar
-    re odernar
+    re ordenar # sp_list
+    juntar # listaConcat
+    re odernar #sp list
     imprimir
              """
 #a =list_creator("maumau")
-a,b=sp_list(input())
+a,b= sp_list("14764")
+c =WordConcat(b,a)
 print(a,b)
