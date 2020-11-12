6c2cdf23be7ac3395fe09bec148b1a63dc5393b8
oliver <mauriliodepaulo@gmail.com>
2020-11-12 18:16:17 -0300

    faze de execuçao iniciada consertos de bugs - criaçao da bibliotecaca - melhoria no codigo faltou: methods de conversao v 0.1.12 build:

--- tests/lista.py
+++ tests/lista.py
@@ -1,21 +1,14 @@
 #quebra de list e relocaçao
+#listar e separa
+a = "uva"
+def sp_list(a):
+ b=a
+ c = int(len(b))
+ e = int(len(b)/2)
+ d = list(b)
+ lado_a = slice(0,e)
+ lado_b = slice(e,c)
+ print(d[lado_a])
+ print(d[lado_b])
 
-a = "abcdefghijklmnopqrstuvwxyz"
-aux = "bsalkb;vkdebvsakuvba;sbgkbg"
-b="miguel" #input()
-c = int(len(b))
-e = int(len(b)/2)
-d = list(b)
-
-for i in b:
-    if i in a:
-      print("tem: ", i)
-    else:
-         pass
-    
-
-lado_a = slice(0,e)
-lado_b = slice(e,c)
-print(d[lado_a])
-print(d[lado_b])
-
+sp_list(a)
---
+++ tests/test.py
@@ -0,0 +1,9 @@
+#list -> bin
+"""
+def listBin(self, parameter_list):
+ for c in a:
+  f = c
+  b.append(bin(f))
+  print(b,"\n", f)
+
+"""
